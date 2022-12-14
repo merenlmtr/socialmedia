3c4960e2ca7c117003c37781c1d0734ea6ce84e1
merenlmtr <merenltr@gmail.com>
2022-12-15 00:03:34 +0530

    necessary environment prepared

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+    console.log("Connected to MongoDB!");
+})
 
 app.listen(8800, () => {
     console.log("Backend Server is running...");

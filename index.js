24ae1a46aff0ebf5a636b5f8d0d20bd78f0d0c90
merenlmtr <merenltr@gmail.com>
2022-12-21 20:50:51 +0530

    Register and Login Model Created

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false); //supressing Mongoose deprecation warning
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
     console.log("Connected to MongoDB!");
+})
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Homepage");
 })
 
 app.listen(8800, () => {

d6e5f02dc7fd9031614c8f67cd33aff894c786eb
merenlmtr <merenltr@gmail.com>
2022-12-22 22:34:49 +0530

    Get a user function added

--- routes/users.js
+++ routes/users.js
@@ -37,6 +37,15 @@ router.delete("/:id", async (req, res) => {
     }
 });
 //Get a User
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, isAdmin, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 //Follow User
 //Unfollow User
 

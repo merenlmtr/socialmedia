42cc42c5df78a4faa9cf10842df56c8ba6f1c935
merenlmtr <merenltr@gmail.com>
2022-12-22 23:01:37 +0530

    UnFollow user function created

--- routes/users.js
+++ routes/users.js
@@ -67,5 +67,25 @@ router.put("/:id/follow", async (req, res) => {
     }
 });
 //Unfollow User
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json("User has been unfollowed");
+            } else {
+                res.status(403).json("You dont follow this user.");
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You cannot unfollow yourself.")
+    }
+});
+
 
 module.exports = router

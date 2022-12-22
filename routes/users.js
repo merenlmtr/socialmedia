679ce990a37faa430a924624aa17efadd2871f04
merenlmtr <merenltr@gmail.com>
2022-12-22 22:13:41 +0530

    Update user function created

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-    res.send("Hey its users route");
+//Update User
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
+            res.status(200).json("Account has been Updated.")
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You do not have permission.");
+    }
 })
+//Delete User
+//Get a User
+//Follow User
+//Unfollow User
 
 module.exports = router
